e631beaedca8a54a32fb18609f04dbbb6fc2b741
albanytech <tom@albanytech.co.uk>
2022-07-12 10:21:04 +0000

    Add files via upload

---
+++ scripts/DetectWSL.ps1
@@ -0,0 +1,6 @@
+IF ( Get-WmiObject -Class Win32_OptionalFeature | Where-Object {($_.Name -Match "Microsoft-Windows-Subsystem-Linux") -and ($_.InstallState -eq 1)} ){
+  $True
+}
+ELSE {
+  $False
+}
