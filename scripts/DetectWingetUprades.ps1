6aaa35324f1216b7c0b9ad2fceeee2310c9667f4
Tom Coleman <tom@albanytech.co.uk>
2022-08-11 21:13:05 +0100

    Use ./winget.exe so it runs in system context

--- scripts/DetectWingetUprades.ps1
+++ scripts/DetectWingetUprades.ps1
@@ -1,6 +1,19 @@
 #Script checks for Winget Upgrades on Windows 11.
 
-if ((winget upgrade) -eq ("No installed package found matching input criteria.")) {
+##Find Winget Path
+
+$ResolveWingetPath = Resolve-Path "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe"
+    if ($ResolveWingetPath){
+           $WingetPath = $ResolveWingetPath[-1].Path
+    }
+
+$config
+
+##Navigate to the Winget Path
+Set-Location $wingetpath
+
+if ((.\winget.exe upgrade) -eq ("No installed package found matching input criteria."))
+{
 	Write-Host "Nothing needs upgrading"
 	exit 0
 } Else {
--- scripts/RemediateWingetUpgrades.ps1
+++ scripts/RemediateWingetUpgrades.ps1
@@ -1,7 +1,19 @@
 #Script upgrades all available winget packages
 
+##Find Winget Path
+
+$ResolveWingetPath = Resolve-Path "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe"
+    if ($ResolveWingetPath){
+           $WingetPath = $ResolveWingetPath[-1].Path
+    }
+
+$config
+
+##Navigate to the Winget Path
+Set-Location $wingetpath
+
 try{
-    winget upgrade --all
+    ./winget.exe upgrade --all
     
     }
     catch{
