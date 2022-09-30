00ca6b668494b80290466911bef1bb8383fee0f3
Tom Coleman <tom@albanytech.co.uk>
2022-09-30 20:59:07 +0100

    new way of getting winget in system context

--- scripts/DetectWingetUprades.ps1
+++ scripts/DetectWingetUprades.ps1
@@ -1,8 +1,20 @@
 #Script checks for Winget Upgrades on Windows 11.
 
-$Winget = Get-ChildItem -Path (Join-Path -Path (Join-Path -Path $env:ProgramFiles -ChildPath "WindowsApps") -ChildPath "Microsoft.DesktopAppInstaller*_x64*\winget.exe")
+#$Winget = Get-ChildItem -Path (Join-Path -Path (Join-Path -Path $env:ProgramFiles -ChildPath "WindowsApps") -ChildPath "Microsoft.DesktopAppInstaller*_x64*\winget.exe")
 
-if ($(&$winget upgrade) -gt 3) {
+##Find Winget Path
+
+$ResolveWingetPath = Resolve-Path "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe"
+    if ($ResolveWingetPath){
+           $WingetPath = $ResolveWingetPath[-1].Path
+    }
+
+$config
+
+##Navigate to the Winget Path
+cd $wingetPath
+
+if ((./winget.exe upgrade) -gt 3) {
 	Write-Host "Upgrade(s) available."
 	exit 1 # upgrade available, remediation needed
 }
